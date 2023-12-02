d30fa96c7445224cf94c7021345c1f852a4d4b6e
Rashmi Dongre <dongrerashmi15@gmail.com>
2023-12-03 04:41:38 +0530

    frist section complter

---
+++ script.js
@@ -0,0 +1,10 @@
+const image = document.querySelector('.bdo_imag');
+
+    window.addEventListener('scroll', () => {
+      const scrollPosition = window.scrollY;
+
+      // You can adjust the scroll range and scale factor as needed
+      const scaleFactor = 1 + scrollPosition / 7000;
+
+      image.style.transform = `scale(${scaleFactor})`;
+    });
